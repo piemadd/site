edb8766fbbafb8640fd1082fb9322bfab49d23d3
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-12-14 10:41:47 -0600

    added jest and back to top link

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-import '../styles/style.css';
+const mainSection = document.getElementById('mainSection');
+const topLinks = document.getElementById('topLinks');
+
+//code for "back to top"
+
+//initial load
+if (mainSection.scrollTop > 0) topLinks.style.opacity = '1';
+else topLinks.style.opacity = '0';
+
+//whenever user scrolls
+mainSection.addEventListener("scroll", (event) => {
+  if (event.target.scrollTop > 0) topLinks.style.opacity = '1';
+  else topLinks.style.opacity = '0';
+});
+
